8af85dfcd68c7e6ef7eb37d34c5659af36b5c210
turpin_l <turpin_l@etna-alternance.net>
2024-06-10 16:05:14 +0200

    feat: setup react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './app.tsx'
+
+ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
