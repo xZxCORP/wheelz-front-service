03e4cc1226565ee7fd2a8a4fdd0a92792c1a8202
turpin_l <turpin_l@etna-alternance.net>
2024-10-21 18:54:25 +0200

    feat: finish data

--- src/pages/admin/TransactionsTable.tsx
+++ src/pages/admin/TransactionsTable.tsx
@@ -1,14 +1,46 @@
 import { createColumnHelper } from '@tanstack/react-table';
-import { type VehicleTransaction } from '@zcorp/shared-typing-wheelz';
+import { type TransactionAction, type VehicleTransaction } from '@zcorp/shared-typing-wheelz';
+import { useCallback } from 'react';
 
 import { transactionTsr } from '../../clients/api/transaction.api';
 import { Table } from '../../components/admin/Table';
+import { Badge } from '../../components/shared/badge/Badge';
 import { formatDate } from '../../utils/date';
 export const TransactionsTable = () => {
   const { data } = transactionTsr.transactions.getTransactions.useQuery({
     queryKey: ['transactions'],
   });
   const columnHelper = createColumnHelper<VehicleTransaction>();
+  const actionToColor = useCallback((action: TransactionAction) => {
+    switch (action) {
+      case 'create': {
+        return 'success';
+      }
+      case 'update': {
+        return 'warning';
+      }
+      case 'delete': {
+        return 'error';
+      }
+      default: {
+        return 'success';
+      }
+    }
+  }, []);
+  const statusToColor = useCallback((status: string) => {
+    switch (status) {
+      case 'pending': {
+        return 'warning';
+      }
+      case 'finished': {
+        return 'warning';
+      }
+
+      default: {
+        return 'success';
+      }
+    }
+  }, []);
   const columns = [
     columnHelper.accessor('id', {
       header: 'ID',
@@ -16,15 +48,23 @@ export const TransactionsTable = () => {
     }),
     columnHelper.accessor('action', {
       header: 'Action',
-      cell: (info) => info.getValue(),
+      cell: (info) => (
+        <Badge badgeStyle={{ color: actionToColor(info.getValue()) }} text={info.getValue()} />
+      ),
     }),
     columnHelper.accessor('status', {
       header: 'Statut',
-      cell: (info) => info.getValue(),
+      cell: (info) => (
+        <Badge badgeStyle={{ color: statusToColor(info.getValue()) }} text={info.getValue()} />
+      ),
     }),
 
     columnHelper.accessor('dataSignature.signAlgorithm', {
-      header: 'Alorithme de signature',
+      header: 'Algorithme de signature',
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor('data.vin', {
+      header: 'Vin du vehicle',
       cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('timestamp', {
