154827b1367d0e02c7133238c38f13a3e39fe3f6
Léo Turpin <turpin_l@etna-alternance.net>
2025-01-13 18:03:12 +0100

    feat: finish view transaction update

--- src/components/transaction/cards/ViewUpdateTransaction.tsx
+++ src/components/transaction/cards/ViewUpdateTransaction.tsx
@@ -1,7 +1,10 @@
 import type { UpdateVehicleTransactionData, Vehicle } from '@zcorp/shared-typing-wheelz';
 
-import { vehicleInfosLabels } from '../../../types/vehicleLabels';
+import { vehicleFeaturesLabels, vehicleInfosLabels } from '../../../types/vehicleLabels';
 import { Card, CardContent, CardHeader, CardTitle } from '../../shared/Card';
+import { InfoField } from '../../shared/InfoField';
+import { HistoryTable } from '../tables/HistoryTable';
+import { TechnicalControlTable } from '../tables/TechnicalControlTable';
 
 type Props = {
   data: UpdateVehicleTransactionData;
@@ -9,27 +12,46 @@ type Props = {
 export const ViewUpdateTransaction = ({ data }: Props) => {
   return (
     <div className="flex w-full flex-col gap-4">
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Informations générales</CardTitle>
-        </CardHeader>
-        <CardContent className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          {data.changes.infos &&
-            Object.entries(data.changes.infos).map(([key, value]) => {
+      {!!data.changes.infos && (
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Informations générales</CardTitle>
+          </CardHeader>
+          <CardContent className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+            {Object.entries(data.changes.infos).map(([key, value]) => {
               const typedKey = key as keyof Vehicle['infos'];
-              return (
-                <div key={key} className="flex w-full gap-4">
-                  <p className="rounded-md bg-secondary-700 p-2 text-secondary-200">
-                    {vehicleInfosLabels[typedKey]}
-                  </p>
-                  <p className="rounded-md bg-secondary-700 p-2 font-bold text-secondary-200">
-                    {value}
-                  </p>
-                </div>
-              );
+              return <InfoField key={key} label={vehicleInfosLabels[typedKey]} value={value} />;
             })}
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+      )}
+      {!!data.changes.features && (
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Informations techniques</CardTitle>
+          </CardHeader>
+          <CardContent className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+            {Object.entries(data.changes.features).map(([key, value]) => {
+              const typedKey = key as keyof Vehicle['features'];
+              return <InfoField key={key} label={vehicleFeaturesLabels[typedKey]} value={value} />;
+            })}
+          </CardContent>
+        </Card>
+      )}
+      {!!data.changes.history && (
+        <Card className="w-full">
+          <CardContent>
+            <HistoryTable data={data.changes.history} />
+          </CardContent>
+        </Card>
+      )}
+      {!!data.changes.technicalControls && (
+        <Card className="w-full">
+          <CardContent>
+            <TechnicalControlTable data={data.changes.technicalControls} />
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 };
