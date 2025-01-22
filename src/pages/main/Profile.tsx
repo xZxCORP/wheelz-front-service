e8ee4a8e35ad5470ca2bca1d85a1d5ba0bb398cd
clément VITTE <clementvitte@MacBook-Pro-de-clement.local>
2025-01-22 10:10:36 +0100

    add update to profile page

--- src/pages/main/Profile.tsx
+++ src/pages/main/Profile.tsx
@@ -1,7 +1,45 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { type UserUpdate, userUpdateSchema } from '@zcorp/wheelz-contracts';
+import { useForm } from 'react-hook-form';
+
+import { userTsr } from '../../clients/api/user.api';
+import { Button } from '../../components/shared/button/Button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../../components/shared/form/Form';
+import { Input } from '../../components/shared/form/Input';
 import { useAuthStore } from '../../stores/useAuthStore';
+import { useSnackbarStore } from '../../stores/useSnackbar';
 
 export const Profile = () => {
-  const { user } = useAuthStore();
+  const { user, setUser } = useAuthStore();
+  const { addSnackbar } = useSnackbarStore();
+
+  const form = useForm<UserUpdate>({
+    resolver: zodResolver(userUpdateSchema),
+    mode: 'onChange',
+    defaultValues: {
+      firstname: user?.firstname ?? '',
+      lastname: user?.lastname ?? '',
+    },
+  });
+  const { mutate, isPending } = userTsr.users.updateUser.useMutation({
+    onSuccess: (result) => {
+      addSnackbar(result.body.message, 'success');
+      if (user) {
+        setUser({
+          ...user,
+          firstname: form.getValues('firstname')!,
+          lastname: form.getValues('lastname')!,
+        });
+      }
+    },
+  });
 
   if (!user) {
     return (
@@ -12,25 +50,64 @@ export const Profile = () => {
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <Form {...form}>
       <h1 className="mb-6 text-3xl font-bold">
         Profil de {user.firstname} {user.lastname}
       </h1>
-
       <div className="rounded-lg bg-white p-6 shadow-md">
         <h2 className="mb-4 text-xl font-semibold">Informations personnelles</h2>
-        <ul className="space-y-2">
-          <li>
-            <strong>Prénom :</strong> {user.firstname}
-          </li>
-          <li>
-            <strong>Nom :</strong> {user.lastname}
-          </li>
-          <li>
-            <strong>Email :</strong> {user.email}
-          </li>
-        </ul>
+        <form
+          onSubmit={form.handleSubmit((data) =>
+            mutate({
+              body: {
+                firstname: data.firstname,
+                lastname: data.lastname,
+              },
+              params: {
+                id: user.id.toString(),
+              },
+            })
+          )}
+          className="flex flex-col items-center justify-center gap-6"
+        >
+          <FormField
+            control={form.control}
+            name="firstname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Prénom</FormLabel>
+                <FormControl>
+                  <Input placeholder="Prénom" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="lastname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nom</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nom" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input value={user.email} readOnly />
+            </FormControl>
+          </FormItem>
+          <Button type="submit" disabled={isPending}>
+            {isPending ? 'En cours...' : 'Mettre à jour'}
+          </Button>
+        </form>
       </div>
-    </div>
+    </Form>
   );
 };
