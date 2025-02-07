27403e581ff3adfd40beca62987c384f378a2abf
Léo Turpin <turpin_l@etna-alternance.net>
2025-02-07 17:48:37 +0100

    fix: lint

--- src/components/shared/form/Checkbox.tsx
+++ src/components/shared/form/Checkbox.tsx
@@ -11,6 +11,7 @@ const Checkbox = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
+    // eslint-disable-next-line tailwindcss/no-custom-classname
     className={clsx(
       'data-[state=checked]:text-primary-foreground peer size-4 shrink-0 rounded-sm border border-primary-400 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-400',
       className
