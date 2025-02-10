170a9233eb15e8ca2e2eb61a796c041bd9c997ba
Léo Turpin <turpin_l@etna-alternance.net>
2025-02-10 18:54:52 +0100

    feat: better front when no vehicles or single page

--- src/pages/dashboard/client/MyGarage.tsx
+++ src/pages/dashboard/client/MyGarage.tsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigate } from 'react-router-dom';
 
 import { chainTsr } from '../../../clients/api/chain.api';
 import { PaginationControls } from '../../../components/admin/PaginationControls';
+import { Button } from '../../../components/shared/button/Button';
 import { ErrorContainer } from '../../../components/shared/error/ErrorContainer';
 import { usePagination } from '../../../hooks/usePagination';
 import { isApiResponse } from '../../../utils/errors';
@@ -36,14 +37,22 @@ export const MyGarage = () => {
           {allVehiclesData.body.items.map((vehicle) => (
             <MyCarCard key={vehicle.vin} id={vehicle.vin} vehicle={vehicle} onClick={handleCards} />
           ))}
-          <PaginationControls
-            onNextPage={onNextPage}
-            onPreviousPage={onPreviousPage}
-            canNextPage={canNextPage(allVehiclesData.body.meta.total)}
-            canPreviousPage={canPreviousPage()}
-            currentPage={apiPagination.page}
-            totalPages={pagesCount(allVehiclesData.body.meta.total)}
-          />
+          {allVehiclesData.body.meta.page > 1 && (
+            <PaginationControls
+              onNextPage={onNextPage}
+              onPreviousPage={onPreviousPage}
+              canNextPage={canNextPage(allVehiclesData.body.meta.total)}
+              canPreviousPage={canPreviousPage()}
+              currentPage={apiPagination.page}
+              totalPages={pagesCount(allVehiclesData.body.meta.total)}
+            />
+          )}
+          {allVehiclesData.body.meta.total === 0 && (
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p>Vous n&apos;avez pas encore de véhicule dans votre garage.</p>
+              <Button>Ajouter un véhicule</Button>
+            </div>
+          )}
         </div>
         <div className="m-4 overflow-y-scroll rounded bg-primary-100 md:w-3/4">
           <Outlet />
