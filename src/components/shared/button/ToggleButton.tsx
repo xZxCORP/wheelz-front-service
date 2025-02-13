6d799faea583b898ece8a3c5bdc091e3d893d817
Nino <nino@spellz.app>
2025-02-13 23:25:05 +0100

    fix togglebtn

--- src/components/shared/button/ToggleButton.tsx
+++ src/components/shared/button/ToggleButton.tsx
@@ -17,7 +17,7 @@ export const ToggleButton = ({ id, Icon }: Props) => {
   }, [id, location.pathname]);
   return (
     <Button
-      className={`${active ? 'pointer-events-none bg-secondary-500 font-semibold text-white' : 'bg-primary-100 text-black'}  size-14 transition-transform hover:scale-105  hover:bg-primary-50 active:bg-primary-500`}
+      className={`${active ? 'bg-secondary-500 pointer-events-none font-semibold text-white' : 'bg-primary-200 text-black'}  hover:bg-primary-50 active:bg-primary-500 size-14  transition-transform hover:scale-105`}
       asChild
     >
       <Link to={id}>
