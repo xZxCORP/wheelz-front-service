9493437fe21463a9730899f019fb9ed6c15e8359
Nino <nino@spellz.app>
2025-02-13 15:20:27 +0100

    garage card done

--- src/pages/dashboard/client/GarageCard.tsx
+++ src/pages/dashboard/client/GarageCard.tsx
@@ -1,4 +1,5 @@
-import { PiCarThin } from 'react-icons/pi';
+import { GoZap } from 'react-icons/go';
+import { PiCarThin, PiMotorcycleThin } from 'react-icons/pi';
 
 type GarageType = 'auto' | 'moto' | 'controle technique';
 
@@ -16,11 +17,21 @@ export const GarageCard = ({ name, tags }: Props) => {
     <button className="m-5 h-1/3 w-full rounded bg-white py-2 backdrop-blur transition-all hover:scale-105">
       <div className="ml-2 flex gap-2">
         {tags.map((tag) => (
-          <div key={tag} className="bg-primary-400 px- cursor-pointer rounded">
-            {tag === 'auto' ? <PiCarThin size={25} /> : <PiCarThin size={25} />}
+          <div
+            key={tag}
+            className="bg-secondary-500 cursor-pointer rounded-lg px-2 py-1 text-white"
+          >
+            {tag === 'auto' ? (
+              <PiCarThin size={25} />
+            ) : tag === 'moto' ? (
+              <PiMotorcycleThin size={25} />
+            ) : (
+              <GoZap size={25} />
+            )}
           </div>
         ))}
       </div>
+      <hr className="my-3 h-1 w-3/4 justify-self-center border-t" />
       <div className="m-3 flex size-full flex-col">
         <h2 className="font-semibold">{name}</h2>
         <p>Situé à 2.3km</p>
