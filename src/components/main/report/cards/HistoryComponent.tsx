b63b5f45b8e19cacdc9a82d3a7b8753100d01db1
Nino <nino@spellz.app>
2025-02-25 16:30:05 +0100

    added a target blank for the future pdf

---
+++ src/components/main/report/cards/HistoryComponent.tsx
@@ -0,0 +1,109 @@
+import type { Vehicle } from '@zcorp/shared-typing-wheelz';
+import { useState } from 'react';
+import { FaInfoCircle } from 'react-icons/fa';
+import { PiFileArrowUpThin } from 'react-icons/pi';
+
+import { useModal } from '../../../../hooks/useModal';
+import { MODAL_IDS } from '../../../../types/modalIds';
+import { Button } from '../../../shared/button/Button';
+import { Modal } from '../../../shared/Modal';
+
+export const HistoryComponent = ({ vehicle }: { vehicle: Vehicle }) => {
+  const { open, isOpen, close } = useModal(MODAL_IDS.HISTORY);
+  const [currentPdfFile, setCurrentPdfFile] = useState<string | null>(null);
+
+  const getCurrentPdfFile = () => {
+    setCurrentPdfFile('https://12-25-1212-response-aws.minio');
+    open();
+  };
+
+  const handleCloseModal = () => {
+    setCurrentPdfFile(null);
+    close();
+  };
+
+  return (
+    <div>
+      <div className="mt-6 w-full rounded-lg border bg-white p-4 shadow-md">
+        <h2 className="text-lg ">Historique</h2>
+        <p className="mb-4 text-sm text-gray-600">Quel est l&apos;historique pour ce véhicule ?</p>
+
+        {/* Note */}
+        <div className="mb-4 flex items-start rounded-lg bg-gray-100 p-3">
+          <div className="mr-3 shrink-0 text-xl text-secondary-500">
+            <FaInfoCircle />
+          </div>
+          <div>
+            <p className="text-sm text-gray-800">
+              Vérifiez que l&apos;historique de ce véhicule correspondent aux déclarations
+            </p>
+          </div>
+        </div>
+        <h3 className="mb-4 text-xl font-semibold text-gray-800">Historique</h3>
+        <div className="rounded-lg border bg-white p-6 shadow-md">
+          <ul className="space-y-4">
+            {/* Première ligne spéciale pour le dernier événement */}
+            {[
+              ...vehicle.history.map((entry) => ({
+                date: entry.date,
+                type: `Historique : ${entry.type}`,
+              })),
+              ...vehicle.technicalControls.map((control) => ({
+                date: control.date,
+                type: `Contrôle technique : ${control.nature} (${control.result}) - ${control.resultRaw}`,
+              })),
+              vehicle.sinisterInfos.lastSinisterDate
+                ? {
+                    date: vehicle.sinisterInfos.lastSinisterDate,
+                    type: 'Sinistre : Dernier sinistre enregistré',
+                  }
+                : undefined,
+              vehicle.sinisterInfos.lastResolutionDate
+                ? {
+                    date: vehicle.sinisterInfos.lastResolutionDate,
+                    type: 'Sinistre : Dernière résolution de sinistre',
+                  }
+                : undefined,
+            ]
+              .filter((entry) => entry !== undefined)
+              .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+              .map((entry, index) => (
+                <li
+                  key={index}
+                  className={`flex cursor-pointer items-start space-x-1 transition-all hover:translate-x-1 ${
+                    index === 0 ? 'rounded-lg border border-secondary-500 bg-secondary-100 p-4' : ''
+                  }`}
+                  onClick={() => getCurrentPdfFile()} //TODO pass the args to get the file from s3
+                >
+                  <div className={`text-gray-500 ${index === 0 ? 'text-lg ' : ''}`}>
+                    <strong>Date :</strong> {entry.date}
+                  </div>
+                  <div className={`text-gray-800 ${index === 0 ? 'text-lg' : ''}`}>
+                    <strong>Événement :</strong> {entry.type}
+                  </div>
+                </li>
+              ))}
+          </ul>
+
+          <Modal isOpen={isOpen} onClose={handleCloseModal} title="Télécharger la facture">
+            {currentPdfFile && (
+              <div className="flex items-center space-x-2">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                  href={'https://blogs.mtdv.me/wheelz'}
+                >
+                  <u className="text-secondary-500">{currentPdfFile}</u>
+                </a>
+                <Button>
+                  <PiFileArrowUpThin />
+                </Button>
+              </div>
+            )}
+          </Modal>
+        </div>
+      </div>
+    </div>
+  );
+};
