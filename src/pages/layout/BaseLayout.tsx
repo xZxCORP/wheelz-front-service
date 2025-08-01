2b93535f439317f97ed38cace14661715843fd0c
Léo Turpin <turpin_l@etna-alternance.net>
2025-08-01 10:26:23 +0200

    feat: add Footer component and integrate it into layouts

--- src/pages/layout/BaseLayout.tsx
+++ src/pages/layout/BaseLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { LoginModal } from '../../components/main/auth/login/modals/LoginModal';
 import { RegisterModal } from '../../components/main/auth/register/modals/RegisterModal';
 import { Header } from '../../components/main/MainHeader';
+import { Footer } from '../../components/shared/Footer';
 import { Loader } from '../../components/shared/Loader';
 import { useGlobalLoadingStore } from '../../stores/useGlobalLoadingStore';
 
@@ -11,13 +12,14 @@ export const BaseLayout = () => {
   return (
     <div className="flex min-h-screen w-full flex-col overflow-hidden bg-primary-100">
       <Header />
-      <main className="min-h-screen grow py-4">
+      <main className="flex-1 overflow-hidden">
         <Outlet />
       </main>
 
       {isLoading && <Loader fullScreen size="lg" variant="primary" />}
       <RegisterModal />
       <LoginModal />
+      <Footer />
     </div>
   );
 };
